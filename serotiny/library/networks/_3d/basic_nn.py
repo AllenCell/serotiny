ac191c6b1afd464553ba9c4fc3decc7e77d541ec
Gui Pires <guilherme.pires@alleninstitute.org>
2021-02-01 19:18:54 -0800

    make the basic nn dims work

--- serotiny/library/networks/_3d/basic_nn.py
+++ serotiny/library/networks/_3d/basic_nn.py
@@ -14,41 +14,33 @@ def _conv_layer(in_c, out_c, kernel_size=(3, 3, 3), padding=0):
 
 
 class BasicCNN_3D(nn.Module):
-    def __init__(
-        self,
-        num_classes,
-        in_channels=6,
-        num_layers=4,
-        max_pool_layers={1},
-        dimensions=None
-    ):
+    def __init__(self, in_channels=6, num_classes=5, num_layers=5, max_pool_layers={0, 1, 2}):
         super().__init__()
         self.network_name = "BasicNeuralNetwork_3D"
         self.num_classes = num_classes
         self.mp = nn.MaxPool3d(kernel_size=2, padding=0)
         self.max_pool_layers = max_pool_layers
 
-        out_channels = 4
         layers = []
 
+        out_channels = 4
         for i in range(num_layers):
             layers.append(
-                _conv_layer(in_channels, out_channels, kernel_size=(2, 3, 3))
+               _conv_layer(in_channels, out_channels, kernel_size=(3, 3, 3))
             )
             in_channels = out_channels
-            out_channels = out_channels * (2**(i + 1))
+            out_channels = out_channels * int(1.5**(i))
 
         self.layers = nn.Sequential(*layers)
 
         # hard-coded here because I'm also fixing the input dims elsewhere
         # TODO: change this
-        self.output = nn.Linear(512, num_classes)
+        self.output = nn.Linear(2880, num_classes)
 
     def forward(self, x):
         for i, layer in enumerate(self.layers):
             x = layer(x)
             if i in self.max_pool_layers:
                 x = self.mp(x)
-        print(x.shape)
-        print(x.view(x.shape[0], -1).shape)
+
         return self.output(x.view(x.shape[0], -1))
