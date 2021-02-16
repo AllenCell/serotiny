77741ba3c1229489c4ad673646a5876324199e38
Gui Pires <guilherme.pires@alleninstitute.org>
2021-02-16 11:53:37 -0800

    fixing previous commit

--- serotiny/steps/filter_data.py
+++ serotiny/steps/filter_data.py
@@ -20,7 +20,8 @@ def filter_data(
         dataset_path,
         output_path,
         filter_options,
-        required_fields=None
+        required_fields=None,
+        seed=42
 ):
     if required_fields is None:
         required_fields = {}
@@ -28,6 +29,12 @@ def filter_data(
     dataset = load_csv(
         dataset_path,
         required_fields.get(dataset_path, {}))
+
+    if required_fields is not None:
+        dataset = dataset.dropna(subset=required_fields)
+
+    # trick to shuffle dataset rows
+    dataset = dataset.sample(frac=1, random_state=seed)
 
     output = dataset
     operation = filter_options.get('operation', 'values')
@@ -49,6 +56,26 @@ def filter_data(
             for value in values]
 
         output = pd.concat(subsets)
+    elif operation == 'sample':
+        column = filter_options['column']
+        number = filter_options.get('number', 1)
+        index = filter_options.get('index', 'id')
+
+        dataset.set_index(index)
+        values = dataset[column].unique()
+
+        subsets = []
+        for value in values:
+            class_rows = dataset[dataset[column] == value]
+            subsets.append(
+                class_rows.sample(number, random_state=seed,
+                                  # only sample with replacement if there
+                                  # aren't enough data points in this class
+                                  replace=(len(class_rows) < number))
+            )
+
+        output = pd.concat(subsets)
+
 
     output.to_csv(output_path, index=False)
 
