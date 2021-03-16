e2453d9148d2c61553594c26d0b48e427c9a2e20
Guilherme Pires <mail@gpir.es>
2021-03-15 21:06:25 -0700

    fix splitting logic in dataframe_dataset

--- serotiny/data/dataframe_dataset.py
+++ serotiny/data/dataframe_dataset.py
@@ -23,6 +23,9 @@ class DataframeDataset(Dataset):
 
     def __init__(self, dataframe, loaders=None, iloc=True, split_col=None):
         self.dataframe = dataframe
+        if iloc:
+            self.dataframe = self.dataframe.reset_index()
+
         self.loaders = loaders
         self.iloc = iloc
 
@@ -31,14 +34,15 @@ class DataframeDataset(Dataset):
         self.test_split = None
 
         if split_col is not None:
+            assert iloc
             assert dataframe.dtypes[split_col] == np.dtype("O")
             dataframe[split_col] = dataframe[split_col].str.lower()
             split_names = self.dataframe[split_col].unique().tolist()
-            assert split_names.issubset(["train", "validation", "test"])
+            assert set(split_names).issubset({"train", "validation", "test"})
 
-            self.train_split = dataframe.loc[dataframe[split_col] == "train"].index.tolist()
-            self.val_split = dataframe.loc[dataframe[split_col] == "validation"].index.tolist()
-            self.test_split = dataframe.loc[dataframe[split_col] == "test"].index.tolist()
+            self.train_split = self.dataframe.loc[self.dataframe[split_col] == "train"].index.tolist()
+            self.val_split = self.dataframe.loc[self.dataframe[split_col] == "validation"].index.tolist()
+            self.test_split = self.dataframe.loc[self.dataframe[split_col] == "test"].index.tolist()
 
     def __len__(self):
         return len(self.dataframe)
