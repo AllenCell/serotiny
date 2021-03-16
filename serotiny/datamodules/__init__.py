e6fa417c8cf59ced644212b42e2a9210051ec6a1
Guilherme Pires <mail@gpir.es>
2021-03-15 20:00:09 -0700

    added folder datamodule

--- serotiny/datamodules/__init__.py
+++ serotiny/datamodules/__init__.py
@@ -2,6 +2,7 @@ from .actk.actk2d import ACTK2DDataModule
 from .actk.actk3d import ACTK3DDataModule
 from .aics_mnist import AICS_MNIST_DataModule
 from .dummy import DummyDatamodule
+from .folder_datamodule import FolderDatamodule
 
 __all__ = ["ACTK2DDataModule", "ACTK3DDataModule", "AICS_MNIST_DataModule",
-           "DummyDatamodule"]
+           "DummyDatamodule", "FolderDatamodule"]
