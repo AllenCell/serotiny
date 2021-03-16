5d0e6596147c576c5b6e52f8b28633839f119008
Guilherme Pires <mail@gpir.es>
2021-03-15 19:57:58 -0700

    add ability to split datafrane datasets

--- serotiny/data/dataframe_dataset.py
+++ serotiny/data/dataframe_dataset.py
@@ -1,7 +1,9 @@
 from collections.abc import Iterable
+
+import numpy as np
+
 from torch.utils.data import Dataset
 from torch.utils.data.dataloader import default_collate as collate
-
 
 class DataframeDataset(Dataset):
     """
@@ -17,16 +19,26 @@ class DataframeDataset(Dataset):
     loaders: dict
         A dict which contains methods to appropriately load data from columns
         in the dataset.
-    transform: callable
-        A transform to be applied to the items of this dataset when they are
-        loaded
     """
 
-    def __init__(self, dataframe, loaders=None, transform=None, iloc=True):
+    def __init__(self, dataframe, loaders=None, iloc=True, split_col=None):
         self.dataframe = dataframe
         self.loaders = loaders
-        self.transform = transform
         self.iloc = iloc
+
+        self.train_split = None
+        self.val_split = None
+        self.test_split = None
+
+        if split_col is not None:
+            assert dataframe.dtypes[split_col] == np.dtype("O")
+            dataframe[split_col] = dataframe[split_col].str.lower()
+            split_names = self.dataframe[split_col].unique().tolist()
+            assert split_names.issubset(["train", "validation", "test"])
+
+            self.train_split = dataframe.loc[dataframe[split_col] == "train"].index.tolist()
+            self.val_split = dataframe.loc[dataframe[split_col] == "validation"].index.tolist()
+            self.test_split = dataframe.loc[dataframe[split_col] == "test"].index.tolist()
 
     def __len__(self):
         return len(self.dataframe)
@@ -40,7 +52,6 @@ class DataframeDataset(Dataset):
         return {key: loader(row) for key, loader in self.loaders.items()}
 
     def __getitem__(self, idx):
-        # TODO: look at handling the index as strings
         sample = (
             collate([self._get_single_item(i) for i in idx])
             if (isinstance(idx, Iterable) and not isinstance(idx, str))
