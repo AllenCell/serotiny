b5d13627e8e8d7e57be75bc5f9be215ccc07bd82
Ritvik Vasan <ritvik.vasan@alleninstitute.org>
2021-03-22 15:40:26 -0700

    added a test for Guassian datamodule MLP training

---
+++ tests/test_mlp_vae_step.py
@@ -0,0 +1,29 @@
+from serotiny_steps.train_mlp_vae import train_mlp_vae
+
+
+def test_mlp_vae_step():
+    train_mlp_vae(
+        data_dir="./test_results2/",
+        output_path="./test_results2/",
+        datamodule="GaussianDataModule",
+        batch_size=64,
+        num_gpus=1,
+        num_workers=4,
+        num_epochs=1,
+        lr=1e-3,
+        optimizer="adam",
+        scheduler="reduce_lr_plateau",
+        x_label="gaussian_x",
+        c_label="gaussian_c",
+        c_label_ind="gaussian_c_ind",
+        x_dim=2,
+        c_dim=4,
+        enc_layers=[2, 256, 256, 64],
+        dec_layers=[64, 256, 256, 2],
+        beta=1,
+        length=64 * 100,  # kwarg for Gaussian
+    )
+
+
+if __name__ == "__main__":
+    test_mlp_vae_step()
