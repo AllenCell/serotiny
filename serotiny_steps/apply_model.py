7cbeb144806ba5bdf2e6fa7f3cb154c16f5fbc72
Guilherme Pires <mail@gpir.es>
2021-03-23 17:21:49 -0700

    load-store model logic + apply_model step

---
+++ serotiny_steps/apply_model.py
@@ -0,0 +1,69 @@
+from typing import Union, Optional, Dict, Tuple
+from pathlib import Path
+import fire
+
+import torch
+import pandas as pd
+
+from serotiny.datamodules import FolderDatamodule
+from serotiny.models.zoo import get_model
+
+def store_batch(batch, result, target_folder, mode):
+    target_folder = Path(target_folder)
+    if mode == "csv":
+        df = pd.DataFrame(result)
+        df["basename"] = batch["basename"]
+        fpath = (target_folder / "results.csv")
+        if fpath.exists():
+            df.to_csv(fpath, header=False, mode="a")
+        else:
+            df.to_csv(fpath, header=False, mode="a")
+    else:
+        for field_name, tensors in result.items():
+            for basename, tensor in zip(batch["basename"], tensors):
+                torch.save(tensor, target_folder / f"{basename}_{field_name}.pt")
+
+def apply_model(
+    model_path: str,
+    data_path: Union[str, Path],
+    target_path: Union[str, Path],
+    batch_size_dl: int,
+    num_workers_dl: int,
+    store_mode: str,
+    gpu_id: Optional[int] = None,
+    model_root: Optional[Union[str, Path]] = None,
+    manifest_path: Optional[Union[str, Path]] = None,
+    loader_dict: Optional[Dict[str, Tuple[str, Dict[str, Dict]]]] = None,
+):
+
+    if store_mode not in ("csv", "file"):
+        raise ValueError("`store_mode` must be either 'csv' or 'file'")
+
+    data_path = Path(data_path)
+    model = get_model(model_path, model_root)
+    model.eval()
+
+    dm = FolderDatamodule(
+        batch_size=batch_size_dl,
+        num_workers=num_workers_dl,
+        path=data_path,
+        train_frac=1.0,
+        loader_dict=loader_dict,
+        return_paths=True,
+    )
+
+    if gpu_id is not None:
+        torch.cuda.set_device(gpu_id)
+        model = model.cuda()
+
+    with torch.no_grad():
+        for batch in dm.train_dataloader():
+            if gpu_id is not None:
+                batch = batch.cuda()
+
+            result = model(batch)
+            if gpu_id is not None:
+                batch = batch.cpu()
+                result = result.cpu()
+
+            store_batch(batch, result, target_path, store_mode)
