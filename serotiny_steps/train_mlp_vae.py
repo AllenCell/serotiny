4b0c737d8244bca747d92941885d71faad255ae0
Ritvik Vasan <ritvik.vasan@alleninstitute.org>
2021-04-05 14:06:55 -0700

    added new callbacks to train step

--- serotiny_steps/train_mlp_vae.py
+++ serotiny_steps/train_mlp_vae.py
@@ -8,6 +8,7 @@ from datetime import datetime
 
 import fire
 import pytorch_lightning as pl
+import numpy as np
 import yaml
 from pytorch_lightning.loggers import TensorBoardLogger, CSVLogger
 from pytorch_lightning.callbacks import ModelCheckpoint, GPUStatsMonitor, EarlyStopping
@@ -18,18 +19,24 @@ from serotiny.networks.vae import CBVAEEncoderMLPResnet, CBVAEDecoderMLPResnet
 from serotiny.models import CBVAEMLPModel
 
 import serotiny.datamodules as datamodules
-from serotiny.models.callbacks import MLPVAELogging, SpharmLatentWalk
+from serotiny.models.callbacks import (
+    MLPVAELogging,
+    SpharmLatentWalk,
+    GetEmbeddings,
+    GetClosestCellsToDims,
+)
 
 log = logging.getLogger(__name__)
 pl.seed_everything(42)
 
 
 def train_mlp_vae(
-    data_dir: str,
+    source_path: str,
+    modified_source_save_dir: str,
     output_path: str,
     datamodule: str,
     batch_size: int,
-    num_gpus: int,
+    gpu_id: str,
     num_workers: int,
     num_epochs: int,
     lr: float,
@@ -40,9 +47,12 @@ def train_mlp_vae(
     c_label_ind: str,
     x_dim: int,
     c_dim: int,
-    enc_layers: list,
-    dec_layers: list,
+    latent_dims: int,
+    hidden_layers: list,
     beta: float,
+    cvapipe_analysis_config_path: str,
+    latent_walk_range: list,
+    n_cells: int,  # No of closets cells to find per location
     length: Optional[int] = None,  # For Gaussian
     corr: Optional[bool] = False,  # For Gaussian
     id_fields: Optional[list] = None,  # For Spharm
@@ -57,6 +67,11 @@ def train_mlp_vae(
     ----------
 
     """
+    import os
+
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
+    os.environ["CUDA_VISIBLE_DEVICES"] = gpu_id
+
     if datamodule not in datamodules.__dict__:
         raise KeyError(
             f"Chosen datamodule {datamodule} not available.\n"
@@ -68,7 +83,7 @@ def train_mlp_vae(
         dm = datamodules.__dict__[datamodule](
             batch_size=batch_size,
             num_workers=num_workers,
-            data_dir=data_dir,
+            data_dir=modified_source_save_dir,
             x_label=x_label,
             c_label=c_label,
             c_label_ind=c_label_ind,
@@ -81,7 +96,7 @@ def train_mlp_vae(
         dm_no_shuffle = datamodules.__dict__[datamodule](
             batch_size=batch_size,
             num_workers=num_workers,
-            data_dir=data_dir,
+            data_dir=modified_source_save_dir,
             x_label=x_label,
             c_label=c_label,
             c_label_ind=c_label_ind,
@@ -94,7 +109,8 @@ def train_mlp_vae(
         dm = datamodules.__dict__[datamodule](
             batch_size=batch_size,
             num_workers=num_workers,
-            data_dir=data_dir,
+            source_path=source_path,
+            modified_source_save_dir=modified_source_save_dir,
             x_label=x_label,
             c_label=c_label,
             c_label_ind=c_label_ind,
@@ -106,18 +122,22 @@ def train_mlp_vae(
         dm.prepare_data()
         dm.setup()
 
+    hidden_layers_tmp = hidden_layers.copy()
+    print(hidden_layers, hidden_layers_tmp)
     encoder = CBVAEEncoderMLP(
         x_dim=x_dim,
         c_dim=c_dim,
-        enc_layers=enc_layers,
+        hidden_layers=hidden_layers,
+        latent_dims=latent_dims,
     )
-
+    print(hidden_layers, hidden_layers_tmp)
     decoder = CBVAEDecoderMLP(
         x_dim=x_dim,
         c_dim=c_dim,
-        dec_layers=dec_layers,
+        hidden_layers=hidden_layers_tmp,
+        latent_dims=latent_dims,
     )
-
+    print(hidden_layers, hidden_layers_tmp)
     # encoder = CBVAEEncoderMLPResnet(
     #     x_dim=x_dim,
     #     c_dim=c_dim,
@@ -140,23 +160,14 @@ def train_mlp_vae(
         c_label_ind=c_label_ind,
     )
 
-    tb_logger = TensorBoardLogger(
-        save_dir=str(output_path) + "/lightning_logs",
-        version="version_" + datetime.now().strftime("%d-%m-%Y--%H-%M-%S"),
-        name="",
-    )
+    print(output_path)
+    tb_logger = TensorBoardLogger(save_dir=str(output_path))
 
     csv_logger = CSVLogger(
-        save_dir=str(output_path) + "/lightning_logs" + "/csv_logs",
-        version="version_" + datetime.now().strftime("%d-%m-%Y--%H-%M-%S"),
-        name="",
+        save_dir=str(output_path) + "/csv_logs",
     )
 
-    ckpt_path = os.path.join(
-        str(output_path) + "/lightning_logs",
-        tb_logger.version,
-        "checkpoints",
-    )
+    ckpt_path = str(output_path) + "/checkpoints"
 
     # Initialize model checkpoint
     checkpoint_callback = ModelCheckpoint(
@@ -176,25 +187,45 @@ def train_mlp_vae(
         ]
     elif datamodule == "VarianceSpharmCoeffs":
 
+        mlp_vae_logging = MLPVAELogging(datamodule=dm, values=values)
+
+        get_embeddings = GetEmbeddings(
+            resample_n=2, x_label=dm.x_label, c_label=dm.c_label, id_fields=dm.id_fields
+        )
+
+        get_closest_cells_to_dims = GetClosestCellsToDims(
+            np.array(latent_walk_range),
+            spharm_coeffs_cols=dm.spharm_cols,
+            metric="euclidean",
+            id_col="CellId",
+            N_cells=n_cells,
+            c_shape=c_dim,
+        )
         config = yaml.load(
-            open(
-                "/allen/aics/modeling/ritvik/projects/cvapipe_analysis/config.yaml", "r"
-            ),
+            open(cvapipe_analysis_config_path, "r"),
             Loader=yaml.FullLoader,
+        )
+
+        spharm_latent_walk = SpharmLatentWalk(
+            config=config,
+            spharm_coeffs_cols=dm.spharm_cols,
+            latent_walk_range=latent_walk_range,
         )
         callbacks = [
             GPUStatsMonitor(),
             GlobalProgressBar(),
-            MLPVAELogging(datamodule=dm, values=values),
-            SpharmLatentWalk(config=config),
-            EarlyStopping("val_loss"),
+            EarlyStopping("val_loss", patience=10),
+            mlp_vae_logging,
+            get_embeddings,
+            get_closest_cells_to_dims,
+            spharm_latent_walk,
         ]
 
     trainer = pl.Trainer(
         logger=[tb_logger, csv_logger],
         accelerator="ddp",
         replace_sampler_ddp=False,
-        gpus=num_gpus,
+        gpus=1,
         max_epochs=num_epochs,
         progress_bar_refresh_rate=5,
         checkpoint_callback=checkpoint_callback,
