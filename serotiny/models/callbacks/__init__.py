54d53b0506a028f2a9f3c3f63fb0872ba426051f
Ritvik Vasan <ritvik.vasan@alleninstitute.org>
2021-04-05 14:06:34 -0700

    added callbacks to init

--- serotiny/models/callbacks/__init__.py
+++ serotiny/models/callbacks/__init__.py
@@ -1,5 +1,7 @@
 from .example_callback import MyPrintingCallback
 from .mlp_vae_logging import MLPVAELogging
 from .spharm_latent_walk import SpharmLatentWalk
+from .get_closest_cells_to_dims import GetClosestCellsToDims
+from .get_embeddings import GetEmbeddings
 
-__all__ = ["MyPrintingCallback", "MLPVAELogging", "SpharmLatentWalk"]
+__all__ = ["MyPrintingCallback", "MLPVAELogging", "SpharmLatentWalk", "GetClosestCellsToDims", "GetEmbeddings"]
