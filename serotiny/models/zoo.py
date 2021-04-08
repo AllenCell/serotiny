821aaaf7cfca335880c369360912d97cb630af7e
Guilherme Pires <mail@gpir.es>
2021-04-07 17:22:33 -0700

    get_trainer_at_checkpoint

--- serotiny/models/zoo.py
+++ serotiny/models/zoo.py
@@ -30,6 +30,21 @@ def get_model(model_path, model_root=None):
 
     return model_class.load_from_checkpoint(checkpoint_path=model_path)
 
+def get_trainer_at_checkpoint(model_path, model_root=None):
+    model_root = _get_root(model_root)
+
+    if not model_root.exists():
+        raise FileNotFoundError("Given model_root does not exists.")
+
+    model_class, model_id = model_path.split("/")
+    model_class = models.__dict__[model_class]
+
+    model_id = (model_id if ".ckpt" in model_id else model_id+".ckpt")
+    model_path = (model_root / model_class) / model_id
+
+    trainer = Trainer(resume_from_checkpoint=model_path)
+
+
 def store_model(trainer, model_class, model_id, model_root=None):
     model_root = _get_root(model_root)
 
