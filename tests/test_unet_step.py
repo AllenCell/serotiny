408d1c54d3c27479c6d1e011925bf7456572444d
Caleb Chan <Calebium@gmail.com>
2021-04-07 17:35:49 -0700

    (1) Moved auto padding calculation to __init__ of UnetModel (2) Added PyTorch profiler to train_unet.py (3) Added precision=16 to trainer in train_unet.py

--- tests/test_unet_step.py
+++ tests/test_unet_step.py
@@ -3,6 +3,17 @@ from serotiny_steps.train_unet import train_unet
 
 def test_unet_step():
     
+    # batch_size = 8, depth = 3, channel_fan_top = 64 (x)
+    # 16-bit precision
+    # batch_size = 8, depth = 3, channel_fan_top = 64 (finished 10 epochs) - 1 (version_31-03-2021--14-33-31)
+    # batch_size = 8, depth = 4, channel_fan_top = 64 (x)
+    # batch_size = 1, depth = 4, channel_fan_top = 64
+    # batch_size = 2, depth = 4, channel_fan_top = 64 (x)
+    # batch_size = 2, depth = 4, channel_fan_top = 32
+    # batch_size = 8, depth = 4, channel_fan_top = 32 (finished 10 epochs) - 2 (version_31-03-2021--17-04-37)
+    # batch_size = 16, depth = 4, channel_fan_top = 32 (memory error)
+    # batch_size = 16, depth = 4, channel_fan_top = 16 (finished 10 epochs) - 3 (version_01-04-2021--16-22-11)
+    
     try:
         train_unet(
             data_dir="/allen/aics/modeling/VariancePlayground/manifests/",
@@ -27,8 +38,9 @@ def test_unet_step():
             output_channels=['nucleus_segmentation'],
             # depth=3,  # Unet-specific
             depth=2,  # Unet-specific
+            channel_fan_top=64,  # Unet-specific
             auto_padding=True,  # Unet-specific
-            channel_fan_top=64,
+            
             # kwargs for dummy image data module
             length=10,
             #input_dims=[572, 572, 60],
