89a2f6bac0e878d9c50dad664dfa6b79c9f07f05
Guilherme Pires <mail@gpir.es>
2021-05-12 11:49:48 -0700

    update apply model step

--- serotiny_steps/apply_model.py
+++ serotiny_steps/apply_model.py
@@ -1,75 +1,46 @@
-from typing import Union, Optional, Dict, Tuple
-from pathlib import Path
+import os
+
+from typing import Dict, List
+
 import fire
+import pytorch_lightning as pl
 
-import torch
-import pandas as pd
-
-from serotiny.datamodules import FolderDatamodule
 from serotiny.models.zoo import get_model
-from serotiny.io.image import tiff_writer
+from serotiny.utils import module_get, get_classes_from_config
 
-def store_batch(batch, result, target_folder, mode, channels):
-    target_folder = Path(target_folder)
-    if mode == "csv":
-        df = pd.DataFrame(result)
-        df["basename"] = batch["basename"]
-        fpath = (target_folder / "results.csv")
-        if fpath.exists():
-            df.to_csv(fpath, header=False, mode="a")
-        else:
-            df.to_csv(fpath, header=False, mode="a")
-    else:
-        for field_name, tensors in result.items():
-            for basename, tensor in zip(batch["basename"], tensors):
-                tiff_writer(
-                    tensor,
-                    target_folder / f"{basename}_{field_name}.pt",
-                    channels
-                )
 
 def apply_model(
     model_path: str,
-    data_path: Union[str, Path],
-    target_path: Union[str, Path],
-    batch_size_dl: int,
-    num_workers_dl: int,
-    store_mode: str,
-    target_channels: Optional[str] = None,
-    gpu_id: Optional[int] = None,
-    model_root: Optional[Union[str, Path]] = None,
-    manifest_path: Optional[Union[str, Path]] = None,
-    loader_dict: Optional[Dict[str, Tuple[str, Dict[str, Dict]]]] = None,
+    datamodule_name: str,
+    datamodule_config: Dict,
+    model_zoo_config: Dict,
+    trainer_config: Dict,
+    gpu_ids: List[int],
+    callbacks: Dict,
 ):
 
-    if store_mode not in ("csv", "file"):
-        raise ValueError("`store_mode` must be either 'csv' or 'file'")
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
+    os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(gpu_ids)
+    num_gpus = len(gpu_ids)
+    num_gpus = (num_gpus if num_gpus != 0 else None)
 
-    data_path = Path(data_path)
-    model = get_model(model_path, model_root)
-    model.eval()
+    model_zoo_path = model_zoo_config.get("path")
+    model = get_model(model_path, model_zoo_path)
 
-    dm = FolderDatamodule(
-        batch_size=batch_size_dl,
-        num_workers=num_workers_dl,
-        path=data_path,
-        train_frac=1.0,
-        loader_dict=loader_dict,
-        return_paths=True,
+    create_datamodule = module_get(datamodules, datamodule_name)
+    datamodule = create_datamodule(**datamodule_config)
+    datamodule.setup()
+
+    callbacks = get_classes_from_config(callbacks)
+    trainer = pl.Trainer(
+        **trainer_config,
+        callbacks=callbacks,
+        gpus=num_gpus,
     )
 
-    if gpu_id is not None:
-        torch.cuda.set_device(gpu_id)
-        model = model.cuda()
+    trainer.test(model=model,
+                 datamodule=datamodule)
 
-    with torch.no_grad():
-        for batch in dm.train_dataloader():
-            if gpu_id is not None:
-                batch = batch.cuda()
 
-            result = model(batch)
-            if gpu_id is not None:
-                batch = batch.cpu()
-                result = result.cpu()
-
-            store_batch(batch, result, target_path, store_mode, channels)
+if __name__ == "__main__":
+    fire.Fire(apply_model)
