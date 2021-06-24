71b7015dcf98a5a84a3a9286ad14a93fe0d9e10a
Ryan Spangler <ryan.spangler@gmail.com>
2021-06-24 10:43:44 -0700

    model is on gpu but array is on cpu

--- serotiny/data/predict.py
+++ serotiny/data/predict.py
@@ -23,11 +23,12 @@ def _tile_prediction_recurse(
     ar_in: torch.Tensor,
     dims_max: Union[int, List[int]],
     overlaps: Union[int, List[int]],
+    device: str = 'cuda',
     **predict_kwargs,
 ):
     """Performs piecewise prediction recursively."""
     if tuple(ar_in.shape[1:]) == tuple(dims_max[1:]):
-        ar_out = predictor(ar_in, **predict_kwargs).numpy().astype(np.float32)
+        ar_out = predictor(ar_in.to(device), **predict_kwargs).numpy().astype(np.float32)
         ar_weight = _get_weights(ar_out.shape)
         return ar_out * ar_weight, ar_weight
     dim = None
