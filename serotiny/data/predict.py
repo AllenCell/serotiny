ee11fa5cbeb7110547dd7febfdee1332d4e8ec46
Ryan Spangler <ryan.spangler@gmail.com>
2021-06-24 10:36:30 -0700

    call the predictor directly

--- serotiny/data/predict.py
+++ serotiny/data/predict.py
@@ -27,7 +27,7 @@ def _tile_prediction_recurse(
 ):
     """Performs piecewise prediction recursively."""
     if tuple(ar_in.shape[1:]) == tuple(dims_max[1:]):
-        ar_out = predictor.predict(ar_in, **predict_kwargs).numpy().astype(np.float32)
+        ar_out = predictor(ar_in, **predict_kwargs).numpy().astype(np.float32)
         ar_weight = _get_weights(ar_out.shape)
         return ar_out * ar_weight, ar_weight
     dim = None
