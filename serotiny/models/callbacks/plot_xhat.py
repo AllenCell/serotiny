39f6ac4c99d1fb7d2b3c9810759fd6f6f263a7bf
Guilherme Pires <mail@gpir.es>
2021-08-27 14:43:52 -0700

    add 3d vs 2d mode, and a logits mode

--- serotiny/models/callbacks/plot_xhat.py
+++ serotiny/models/callbacks/plot_xhat.py
@@ -1,12 +1,17 @@
 import torch
+import torch.nn.functional as F
+
 import matplotlib.pyplot as plt
 
 from pytorch_lightning import Callback, Trainer, LightningModule
 from pytorch_lightning.loggers import TensorBoardLogger
 
 class PlotXHat(Callback):
-    def __init__(self, cell_id=None, cell_id_label="cell_id", x_label="image_in"):
+    def __init__(self, cell_id=None, cell_id_label="cell_id", x_label="image_in",
+                 logits=False, mode="3d"):
         self.cell_id = None
+        self.logits = logits
+        self.mode = mode
 
     def on_validation_batch_end(self, trainer, model, outputs, batch, batch_idx, dataloader_idx):
         if trainer.running_sanity_check:
@@ -40,11 +45,17 @@ class PlotXHat(Callback):
              rcl_per_input_dimension,
              kld_per_latent_dimension) = model.forward(x, **forward_kwargs)
 
-
+            if self.logits:
+                xhat = F.sigmoid(xhat)
 
             fig, axs = plt.subplots(1, 2, figsize=(10,5))
-            axs[0].imshow(x[idx].cpu().numpy().squeeze().mean(axis=0))
-            axs[1].imshow(xhat[idx].cpu().numpy().squeeze().mean(axis=0))
+
+            if self.mode == "3d":
+                axs[0].imshow(x[idx].cpu().numpy().squeeze().mean(axis=0))
+                axs[1].imshow(xhat[idx].cpu().numpy().squeeze().mean(axis=0))
+            else:
+                axs[0].imshow(x[idx].cpu().numpy().squeeze())
+                axs[1].imshow(xhat[idx].cpu().numpy().squeeze())
 
             logger.experiment.add_figure("x and xhat", fig,
                                          global_step=trainer.current_epoch)
