dfab01d38eb440ee4585bb1137e3289366693559
Ritvik Vasan <ritvik.vasan@alleninstitute.org>
2021-09-24 15:13:23 -0700

    added graph datamodule to init

--- serotiny/datamodules/__init__.py
+++ serotiny/datamodules/__init__.py
@@ -2,10 +2,12 @@ from .dummy import DummyDatamodule
 from .manifest_datamodule import ManifestDatamodule
 from .patch import PatchDatamodule
 from .split import SplitDatamodule
+from .graph_datamodule import GraphDatamodule
 
 __all__ = [
     "DummyDatamodule",
     "ManifestDatamodule",
     "PatchDatamodule",
     "SplitDatamodule",
+    "GraphDatamodule",
 ]
