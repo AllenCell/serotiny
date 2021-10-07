adb47f57d3c2faf4e480de2796a839ef23e195e4
Guilherme Pires <mail@gpir.es>
2021-10-07 11:21:29 -0700

    added data transform cli and abstract pipeline cli

--- serotiny_steps/cli/transform_dataframe_cli.py
+++ serotiny_steps/cli/transform_dataframe_cli.py
@@ -1,92 +1,116 @@
-from typing import Dict
+from typing import Union, Dict, Sequence, Optional, T
 from pathlib import Path
-
-import logging
+from inspect import Parameter, signature
 from makefun import wraps
 
-import joblib
+import logging
 
 import pandas as pd
 import serotiny.data.dataframe.transforms as df_transforms
 from serotiny.io.dataframe import read_dataframe
+from ._pipeline_cli import PipelineCLI
+from .image_cli.apply_transforms import transform_images
+from .omegaconf_decorator import omegaconf_decorator
+
+PathLike = Union[str, Path]
+OneOrMany = Union[T, Sequence[T]]
+
 
 logger = logging.getLogger(__name__)
 
+def _store_one_df(result, output_path):
+    if output_path.suffix == ".parquet":
+        result.to_parquet(output_path)
+    elif output_path.suffix == ".csv":
+        result.to_csv(output_path)
+    else:
+        raise TypeError("output path must be either .parquet or .csv")
 
-class TransformDataframeCLI:
-    def __init__(self, suffix="", output_path=None):
-        self._output_path = output_path
-        self._suffix = suffix
-        self._result = None
 
-        for transform in [
-            df_transforms.split_dataframe,
-            df_transforms.filter_rows,
-            df_transforms.filter_columns,
-            df_transforms.sample_n_each,
-            df_transforms.append_one_hot,
-            df_transforms.append_labels_to_integers,
-            df_transforms.append_class_weights,
-        ]:
-            self._add_transform(transform)
+def _store_many_dfs(result, output_path):
+    for name, df in result.items():
+        assert isinstance(name, str)
+        assert isinstance(df, pd.DataFrame)
 
-    def _add_transform(self, func):
-        @wraps(func)
-        def wrapper(**kwargs):
-            if self._result is not None:
-                kwargs["dataframe"] = self._result
-            else:
-                kwargs["dataframe"] = read_dataframe(kwargs["dataframe"])
-
-            self._result = func(**kwargs)
-            return self
-
-        setattr(self, func.__name__, wrapper)
-
-    def __str__(self):
-        """
-        Hijack __str__ to store the final result of a pipeline,
-        stored in self._result
-        """
-
-        if self._result is None:
-            raise ValueError("No result stored on pipeline")
-
-        if self._output_path is None:
-            raise ValueError("No output directory")
-
-        self._output_path = Path(self._output_path)
-        if isinstance(self._result, pd.DataFrame):
-            if self._output_path.suffix == "":
-                self._output_path = self._output_path.with_suffix(self._suffix)
-
-            if self._output_path.suffix == ".parquet":
-                self._result.to_parquet(self._output_path)
-            else:
-                if self._output_path.suffix != ".csv":
-                    logger.warning(
-                        f"Unrecognized suffix {self._output_path.suffix}. "
-                        f"Assuming .csv"
-                    )
-
-                self._result.to_csv(self._output_path)
-
-        elif isinstance(self._result, Dict[str, pd.DataFrame]):
-            for name, df in self._result.items():
-                name = name + self._suffix
-                name = Path(name)
-                if name.suffix == ".csv":
-                    df.to_csv(self._output_path / name)
-                elif name.suffix == ".parquet":
-                    df.to_parquet(self._output_path / name)
-                else:
-                    raise ValueError(f"Unexpected suffix: '{name.suffix}'")
+        name = Path(name)
+        if name.suffix == ".csv":
+            df.to_csv(output_path / name)
+        elif name.suffix == ".parquet":
+            df.to_parquet(output_path / name)
+        elif name.suffix == ""
+            df.to_csv(output_path / name.with_suffix(".csv"))
         else:
-            if self._output_path.is_dir():
-                self._output_path = self._output_path / "result.joblib"
-            else:
-                if self._output_path.suffix == "":
-                    self._output_path = self._output_path.with_suffix(self._suffix)
-            joblib.dump(self._result, self._output_path)
+            raise TypeError(f"Unexpected suffix: '{name.suffix}'")
 
-        return ""
+
+def _read_one_or_many(
+    input_manifests: OneOrMany[PathLike],
+    merge_col: Optional[str] = None
+):
+    input_manifest = None
+
+    if isinstance(input_manifests, (str, Path)):
+        input_manifests = [input_manifests]
+
+    for manifest in input_manifests:
+        manifest = Path(manifest)
+        if not manifest.exists():
+            raise FileNotFoundError(f"Given input path {manifest} does not exist")
+        manifest = read_dataframe(manifest)
+
+        if input_manifest is None:
+            input_manifest = manifest
+            continue
+
+        if merge_col is not None:
+            input_manifest = input_manifest.merge(manifest, on=merge_col, how="outer")
+        else:
+            input_manifest = input_manifest.concat(manifest)
+
+    return input_manifest
+
+
+def _dataframe_from_disk(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        func_sig = signature(func)
+        func_arg_names = list(func_sig.parameters.keys())
+
+        base_args = {}
+        for arg_ix, arg_value in enumerate(args):
+            base_args[func_arg_names[arg_ix]] = arg_value
+
+        base_args.update(kwargs)
+
+        if "dataframe" in base_args:
+            if not isinstance(base_args["dataframe"], pd.DataFrame):
+                if base_args["dataframe"] not in (..., "..."):
+                    base_args["dataframe"] = read_dataframe(base_args["dataframe"])
+
+        return func(**base_args)
+
+    return wrapper
+
+
+class TransformDataframeCLI(PipelineCLI):
+    def __init__(self, input_manifests=None, output_path=None):
+        super().__init__(
+            output_path=output_path,
+            transforms=[
+                _dataframe_from_disk(df_transforms.split_dataframe),
+                _dataframe_from_disk(df_transforms.filter_rows),
+                _dataframe_from_disk(df_transforms.filter_columns),
+                _dataframe_from_disk(df_transforms.sample_n_each),
+                _dataframe_from_disk(df_transforms.append_one_hot),
+                _dataframe_from_disk(df_transforms.append_labels_to_integers),
+                _dataframe_from_disk(df_transforms.append_class_weights),
+                omegaconf_decorator(transform_images, "transforms_to_apply")
+            ],
+            store_methods={
+                pd.DataFrame: _store_one_df,
+                dict: _store_many_dfs,
+            }
+        )
+
+        self._result = (None if input_manifests is None else
+                        _read_one_or_many(input_manifests))
