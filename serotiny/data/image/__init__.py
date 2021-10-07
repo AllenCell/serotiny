147d73e5be14d2f1dd07eb0e8fd9b7da6eb9015a
Guilherme Pires <mail@gpir.es>
2021-10-07 11:13:43 -0700

    add align transform

--- serotiny/data/image/__init__.py
+++ serotiny/data/image/__init__.py
@@ -2,3 +2,4 @@ from .pad import ExpandTo, ExpandColumns
 from .swap import SwapAxes
 from .resize import ResizeTo, ResizeBy, CropCenter
 from .project import Project
+from .align import align_image_2d
