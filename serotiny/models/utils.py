08d416a7227f032a94d7ce6f1d78c8b91a8b819f
Guilherme Pires <guilherme.pires@alleninstitute.org>
2021-11-05 09:12:17 -0700

    removed unmaintained code from model utils

--- serotiny/models/utils.py
+++ serotiny/models/utils.py
@@ -6,60 +6,6 @@ import torch.optim as opt
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def matplotlib_imshow(img, one_channel=False):
-    """
-    Plot image via matplotlib's imshow
-    """
-    if one_channel:
-        img = img.mean(dim=0)
-    img = img / 2 + 0.5  # unnormalize
-    npimg = img.cpu().numpy()
-    if one_channel:
-        plt.imshow(npimg, cmap="Greys")
-    else:
-        plt.imshow(np.transpose(npimg, (1, 2, 0)))
-
-
-def images_to_probs(net, images):
-    """
-    Generates predictions and corresponding probabilities from a trained
-    network and a list of images
-    """
-    output = net(images)
-    # convert output probabilities to predicted class
-    _, preds_tensor = torch.max(output, 1)
-    preds = np.squeeze(preds_tensor.cpu().numpy())
-    return preds, [F.softmax(el, dim=0)[i].item() for i, el in zip(preds, output)]
-
-
-def plot_classes_preds(net, images, labels, classes):
-    """
-    Generates matplotlib Figure using a trained network, along with images
-    and labels from a batch, that shows the network's top prediction along
-    with its probability, alongside the actual label, coloring this
-    information based on whether the prediction was correct or not.
-    Uses the "images_to_probs" function.
-    """
-    preds, probs = images_to_probs(net, images)
-
-    labels = list(labels.cpu().numpy())
-    labels = [int(i) for i in labels]
-    # plot the images in the batch, along with predicted and true labels
-    fig = plt.figure(figsize=(20, 15))
-    for idx in np.arange(4):
-        ax = fig.add_subplot(1, 4, idx + 1, xticks=[], yticks=[])
-        matplotlib_imshow(images[idx], one_channel=True)
-        ax.set_title(
-            "{0}, {1:.1f}%\n(label: {2})".format(
-                classes[preds[idx]],
-                probs[idx] * 100.0,
-                classes[labels[idx]],
-            ),
-        )
-    return fig
-
-
 def add_pr_curve_tensorboard(
     logger, classes, class_index, test_probs, test_preds, global_step=0, name="_val"
 ):
