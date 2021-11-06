8416c688b1518a487b9b61868ff3ecc615d6da31
Guilherme Pires <guilherme.pires@alleninstitute.org>
2021-11-05 17:11:29 -0700

    added transform for random df generation

--- serotiny/data/dataframe/transforms.py
+++ serotiny/data/dataframe/transforms.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 
 
-
 def split_dataframe(
     dataframe: pd.DataFrame,
     train_frac: float,
@@ -318,3 +317,21 @@ def append_class_weights(dataframe: pd.DataFrame, column: str):
     weights = [class_weights_dict[e] for e in dataframe[column]]
     dataframe[f"{column}_class_weights"] = weights
     return dataframe
+
+
+def make_random_df(columns: Sequence[str] = list("ABCD"), n_rows: int = 100):
+    """
+    Generate a random dataframe. Useful to test data wrangling pipelines.
+
+    Parameters
+    ----------
+    columns: Sequence[str] = ["A","B","C","D"]
+        List of columns to add to the random dataframe. If none are provided,
+        a dataframe with columns ["A","B","C","D"] is created
+
+    n_rows: int = 100
+        Number of rows to create for the random dataframe
+    """
+
+    data = np.random.randn(n_rows, len(columns))
+    return pd.DataFrame(data, columns=columns)
