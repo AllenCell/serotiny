e579e83f251f5fe49c6a86084158ea3d1bf467d6
Guilherme Pires <guilherme.pires@alleninstitute.org>
2021-11-05 17:10:00 -0700

    fix typo

--- serotiny/io/__init__.py
+++ serotiny/io/__init__.py
@@ -1,4 +1,4 @@
-from serotiy.utils.lazy_import import lazy_import
+from serotiny.utils.lazy_import import lazy_import
 
 image_loader = lazy_import("serotiny.io.image").image_loader
 download_quilt_data = lazy_import("serotiny.io.quilt").download_quilt_data
