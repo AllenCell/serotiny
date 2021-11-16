63416f52d0e101d6a0a712c47f537c9c002e2ddd
Guilherme Pires <guilherme.pires@alleninstitute.org>
2021-11-15 19:48:01 -0800

    removed lazy imports

--- serotiny/io/__init__.py
+++ serotiny/io/__init__.py
@@ -1,4 +1,1 @@
-from serotiny.utils.lazy_import import lazy_import
 
-image_loader = lazy_import("serotiny.io.image").image_loader
-download_quilt_data = lazy_import("serotiny.io.quilt").download_quilt_data
--- serotiny/io/dataframe/__init__.py
+++ serotiny/io/dataframe/__init__.py
@@ -1,13 +1,11 @@
-from serotiny.utils.lazy_import import lazy_import
+from .dataframe_dataset import DataframeDataset
 
-_df_dataset = lazy_import("serotiny.io.dataframe.dataframe_dataset")
-DataframeDataset = _df_dataset.DataframeDataset
-
-_loaders = lazy_import("serotiny.io.dataframe.loaders")
-LoadColumn = _loaders.LoadColumn
-LoadColumns = _loaders.LoadColumns
-LoadClass = _loaders.LoadClass
-Load2DImage = _loaders.Load2DImage
-Load3DImage = _loaders.Load3DImage
+from .loaders import (
+    LoadColumn,
+    LoadColumns,
+    LoadClass,
+    Load2DImage,
+    Load3DImage,
+)
 
 from .readers import read_csv, read_parquet, read_dataframe, filter_columns
