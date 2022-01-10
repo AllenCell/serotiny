519946a4174818292613bc650f018c7bc8bf2a7a
Ritvik Vasan <ritvik.vasan@alleninstitute.org>
2022-01-10 09:37:23 -0800

    added graph vae

--- serotiny/models/graph/__init__.py
+++ serotiny/models/graph/__init__.py
@@ -1,4 +1,3 @@
 from .base_graph import BaseGraph
 from .node_prediction import GraphNodePredictionModel
-from .base_graph_vae import BaseGraphVAE
 from .graph_vae import GraphVAE
--- serotiny/models/graph/graph_vae.py
+++ serotiny/models/graph/graph_vae.py
@@ -1,63 +1,66 @@
-# from typing import Union, Optional, Sequence
+from typing import Union, Optional, Sequence
 
-# import torch
-# import numpy as np
+import torch
+import numpy as np
 
-# from serotiny.networks.gcn import GCN
-# from serotiny.models.vae import BaseVAE
+from serotiny.networks.gcn import GCN
+from serotiny.networks.mlp import MLP
+from serotiny.models.vae import BaseVAE
 
-# Array = Union[torch.Tensor, np.array, Sequence[float]]
+Array = Union[torch.Tensor, np.array, Sequence[float]]
 
 
-# class GraphVAE(BaseVAE):
-#     def __init__(
-#         self,
-#         x_dim: int,
-#         latent_dim: int,
-#         hidden_layers: Sequence[int],
-#         x_label: str,
-#         optimizer: str = "Adam",
-#         lr: float = 1e-3,
-#         beta: float = 1.0,
-#         prior_mode: str = "isotropic",
-#         prior_logvar: Optional[Array] = None,
-#         learn_prior_logvar: bool = False,
-#     ):
-#         encoder = GCN(x_dim, 2 * latent_dim, hidden_layers=hidden_layers,)
+class GraphVAE(BaseVAE):
+    def __init__(
+        self,
+        x_dim: int,
+        latent_dim: int,
+        hidden_layers: Sequence[int],
+        x_label: str,
+        optimizer: str = "Adam",
+        lr: float = 1e-3,
+        beta: float = 1.0,
+        prior_mode: str = "isotropic",
+        prior_logvar: Optional[Array] = None,
+        learn_prior_logvar: bool = False,
+    ):
+        encoder = GCN(x_dim, 2 * latent_dim, hidden_layers=hidden_layers,)
 
-#         decoder = GCN(latent_dim, x_dim, hidden_layers=hidden_layers,)
+        decoder = MLP(latent_dim, x_dim, hidden_layers=hidden_layers,)
 
-#         super().__init__(
-#             encoder=encoder,
-#             decoder=decoder,
-#             latent_dim=latent_dim,
-#             optimizer=optimizer,
-#             x_label=x_label,
-#             lr=lr,
-#             beta=beta,
-#             prior_mode=prior_mode,
-#             prior_logvar=prior_logvar,
-#             learn_prior_logvar=learn_prior_logvar,
-#         )
+        super().__init__(
+            encoder=encoder,
+            decoder=decoder,
+            latent_dim=latent_dim,
+            optimizer=optimizer,
+            x_label=x_label,
+            lr=lr,
+            beta=beta,
+            prior_mode=prior_mode,
+            prior_logvar=prior_logvar,
+            learn_prior_logvar=learn_prior_logvar,
+        )
 
-#     def parse_batch(self, batch, parse_mask):
-#         x = batch.x.float()
-#         edge_index = batch.edge_index
-#         edge_weight = batch.edge_weight
-#         if parse_mask == "train":
-#             x = batch.x[batch.train_mask].float()
-#         elif parse_mask == "val":
-#             x = batch.x[batch.val_mask].float()
-#         elif parse_mask == "test":
-#             x = batch.x[batch.test_mask].float()
+    def parse_batch(self, batch, parse_mask):
+        x = batch.x.float()
+        edge_index = batch.edge_index
+        edge_weight = batch.edge_weight
+        cellid = batch.cellid
+        if parse_mask == "train":
+            mask = batch.train_mask
+        elif parse_mask == "val":
+            mask = batch.val_mask
+        elif parse_mask == "test":
+            mask = batch.test_mask
 
-#         return x, {"edge_index": edge_index, "edge_weight": edge_weight}
+        return x, {"edge_index": edge_index, "edge_weight": edge_weight, "mask": mask, "cellid": cellid}
 
-#     def training_step(self, batch, batch_idx):
-#         self._step("train", batch, batch_idx, logger=True, parse_mask="train")
+    def training_step(self, batch, batch_idx):
+        results = self._step("train", batch, batch_idx, logger=True, parse_mask="train")
+        return results
 
-#     def validation_step(self, batch, batch_idx):
-#         return self._step("val", batch, batch_idx, logger=True, parse_mask="val")
+    def validation_step(self, batch, batch_idx):
+        return self._step("val", batch, batch_idx, logger=True, parse_mask="val")
 
-#     def test_step(self, batch, batch_idx):
-#         return self._step("test", batch, batch_idx, logger=False, parse_mask="test")
+    def test_step(self, batch, batch_idx):
+        return self._step("test", batch, batch_idx, logger=False, parse_mask="test")
