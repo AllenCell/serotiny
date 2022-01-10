0daeb09ae45a6b35c505a1d479d21fc49c7fc27a
Ritvik Vasan <ritvik.vasan@alleninstitute.org>
2022-01-10 09:36:51 -0800

    added synthetic trajectory datamodule

--- serotiny/datamodules/datasets/__init__.py
+++ serotiny/datamodules/datasets/__init__.py
@@ -1,5 +1,8 @@
 from .graph_colony import WholeGraph
+from .trajectory_dataset import TrajectoryDataset, TrajectoriesDataset
 
 __all__ = [
     "WholeGraph",
+    "TrajectoryDataset", 
+    "TrajectoriesDataset",
 ]
