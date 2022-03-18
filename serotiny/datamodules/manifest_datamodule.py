f7a36c70f358956d4293c5bdf4d7bc0e8aa27aa0
Guilherme Pires <guilherme.pires@alleninstitute.org>
2022-03-18 14:55:16 -0700

    added predict datamodule and better interface to pytorch dataloader

--- serotiny/datamodules/manifest_datamodule.py
+++ serotiny/datamodules/manifest_datamodule.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from typing import Callable, Dict, Optional, Sequence, Union
 
 import numpy as np
+import pandas as pd
 import pytorch_lightning as pl
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
 from torch.utils.data import DataLoader
@@ -19,23 +20,31 @@ def _make_single_manifest_splits(manifest_path, loaders, split_column, columns=N
     split_names = dataframe[split_column].unique().tolist()
     assert set(split_names).issubset({"train", "valid", "test"})
 
-    return {
+    datasets = {
         split: DataframeDataset(
             dataframe.loc[dataframe[split_column] == split].copy(), loaders[split]
         )
         for split in ["train", "valid", "test"]
     }
 
+    datasets["predict"] = DataframeDataset(dataframe.copy(), loaders["predict"])
+    return datasets
+
 
 def _make_multiple_manifest_splits(split_path, loaders, columns=None):
     split_path = Path(split_path)
     datasets = {}
+    predict_df = []
 
     for fpath in chain(split_path.glob("*.csv"), split_path.glob("*.parquet")):
         mode = re.findall(r"(.*)\.(?:csv|parquet)", fpath.name)[0]
         dataframe = read_dataframe(fpath, required_columns=columns)
         dataset = DataframeDataset(dataframe, loaders=loaders[mode])
         datasets[mode] = dataset
+        predict_df.append(dataframe.copy())
+
+    predict_df = pd.concat(predict_df)
+    datasets["predict"] = DataframeDataset(predict_df, loaders=loaders["predict"])
 
     return datasets
 
@@ -47,9 +56,21 @@ def _dict_depth(d):
 def _parse_loaders(loaders):
     depth = _dict_depth(loaders)
     if depth == 1:
-        loaders = {split: loaders for split in ["train", "valid", "test"]}
+        loaders = {split: loaders for split in ["train", "valid", "test", "predict"]}
     elif depth != 2:
         raise ValueError(f"Loaders dict should have depth 1 or 2. Got {depth}")
+
+    for k in loaders:
+        if isinstance(loaders[k], str):
+            assert loaders[k] in loaders
+            loaders[k] = loaders[loaders[k]]
+
+    for split in ["train", "valid", "test"]:
+        if split not in loaders:
+            raise ValueError(f"'{split}' missing from loaders dict.")
+
+    if "predict" not in loaders:
+        loaders["predict"] = loaders["test"]
 
     return loaders
 
@@ -65,12 +86,6 @@ class ManifestDatamodule(pl.LightningDataModule):
     path: Union[Path, str]
         Path to a manifest file
 
-    batch_size: int
-        batch size for dataloader
-
-    num_workers: int
-        Number of worker processes for dataloader
-
     loaders: Union[Dict, Loader]
         Loader specifications for each given split.
 
@@ -82,15 +97,13 @@ class ManifestDatamodule(pl.LightningDataModule):
         List of columns to load from the dataset, in case it's a parquet file.
         If None, load everything.
 
-    collate: Optional[Callable] = None
-        Alternative collate function for dataloader
-
-    pin_memory: bool = True
-        Set to true when using GPU, for better performance
-
-    drop_last: bool = False
-        Whether to drop the last batch (in case the given batch size is the only
-        supported)
+    dataloader_kwargs:
+        Additional keyword arguments are passed to the
+        torch.utils.data.DataLoader class when instantiating it (aside from
+        `shuffle` which is only used for the train dataloader).
+        Among these args are `num_workers`, `batch_size`, `shuffle`, etc.
+        See the PyTorch docs for more info on these args:
+        https://pytorch.org/docs/stable/data.html#torch.utils.data.DataLoader
     """
 
     def __init__(
@@ -102,15 +115,11 @@ class ManifestDatamodule(pl.LightningDataModule):
         split_column: Optional[Union[Path, str]] = None,
         columns: Optional[Sequence[str]] = None,
         collate: Optional[Callable] = None,
-        pin_memory: bool = True,
-        shuffle: bool = True,
-        drop_last: bool = False,
-        multiprocessing_context: Optional[str] = None,
+        **dataloader_kwargs,
     ):
 
         super().__init__()
         self.path = path
-        self.multiprocessing_context = multiprocessing_context
 
         # if only one loader is specified, the same loaders are
         # used for all  folds
@@ -134,28 +143,13 @@ class ManifestDatamodule(pl.LightningDataModule):
                 path, loaders, split_column, columns
             )
 
-        # DataLoader arguments
-        self.batch_size = batch_size
-        self.num_workers = num_workers
-        self.pin_memory = pin_memory
-        self.drop_last = drop_last
-        self.shuffle = shuffle
-        self.collate = collate
+        self.dataloader_kwargs = dataloader_kwargs
 
-        self._predict_split = "test"
+    def make_dataloader(self, split):
+        kwargs = dict(**self.dataloader_kwargs)
+        kwargs["shuffle"] = kwargs.get("shuffle", False) and split == "train"
 
-    def make_dataloader(self, split, shuffle=False):
-        shuffle = self.shuffle or shuffle
-        return DataLoader(
-            dataset=self.datasets[split],
-            batch_size=self.batch_size,
-            num_workers=self.num_workers,
-            pin_memory=self.pin_memory,
-            drop_last=self.drop_last,
-            collate_fn=self.collate,
-            multiprocessing_context=self.multiprocessing_context,
-            shuffle=(shuffle if split == "train" else False),
-        )
+        return DataLoader(dataset=self.datasets[split], **kwargs)
 
     def train_dataloader(self):
         return self.make_dataloader("train")
@@ -167,4 +161,4 @@ class ManifestDatamodule(pl.LightningDataModule):
         return self.make_dataloader("test")
 
     def predict_dataloader(self):
-        return self.make_dataloader(self._predict_split, shuffle=False)
+        return self.make_dataloader("predict")
