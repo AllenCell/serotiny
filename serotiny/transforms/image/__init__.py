be16b81a3f22d978c4a7e5e4cc63e941e773e855
Guilherme Pires <guilherme.pires@alleninstitute.org>
2022-03-17 18:40:30 -0700

    remove unnused files

--- serotiny/transforms/image/__init__.py
+++ serotiny/transforms/image/__init__.py
@@ -1,17 +1,5 @@
 from .pad import ExpandTo, ExpandColumns, PadTo
 from .swap import SwapAxes
-from .resize import ResizeTo, ResizeBy, CropCenter
+from .crop import CropCenter
 from .project import Project
-from .align import align_image_2d
 from .normalize import NormalizeMinMax, NormalizeAbsolute, NormalizeMean
-
-
-from .feature_extraction import (
-    angle,
-    min_max,
-    percentile,
-    bbox,
-    dillated_bbox,
-    center_of_mass,
-    shcoeffs,
-)
--- serotiny/transforms/image/crop.py
+++ serotiny/transforms/image/crop.py
@@ -1,33 +1,4 @@
-from typing import Sequence
-
-from aicsimageprocessing.resize import resize_to, resize
 from .pad import PadTo
-
-
-class ResizeTo:
-    def __init__(self, target_dims):
-        self.target_dims = target_dims
-
-    def __call__(self, img):
-        return resize_to(img, self.target_dims)
-
-
-class ResizeBy:
-    def __init__(self, factor, method="nearest"):
-        self.method = method
-        self.factor = factor
-
-    def __call__(self, img):
-        if isinstance(self.factor, Sequence):
-            factor = self.factor
-            assert len(factor) == len(img.shape[1:])
-        elif isinstance(self.factor, (int, float)):
-            n_dims = len(img.shape[1:])
-            factor = (1, *(n_dims * [self.factor]))
-        else:
-            raise TypeError(f"Unexpected factor, of type {type(factor)}")
-
-        return resize(img, factor, self.method)
 
 
 class CropCenter:
