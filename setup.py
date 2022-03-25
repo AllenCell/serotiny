5d92a5abd5d6fcb9576d4e527d6d98926ec88d4f
Guilherme Pires <guilherme.pires@alleninstitute.org>
2022-03-25 16:42:45 -0700

    valid version

--- setup.py
+++ setup.py
@@ -69,6 +69,6 @@ setup(
     url="https://github.com/AllenCellModeling/serotiny",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.0",
+    version="0.0.a0",
     zip_safe=False,
 )
