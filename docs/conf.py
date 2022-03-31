d315135c273823277bdd14c875de23685629985f
Guilherme Pires <guilherme.pires@alleninstitute.org>
2022-03-31 15:29:22 -0700

    added logo

--- docs/conf.py
+++ docs/conf.py
@@ -123,6 +123,7 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+html_logo = "serotiny_logo.svg"
 
 # -- Options for HTMLHelp output ---------------------------------------
 
