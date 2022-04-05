a404bdc7294a6132152bace2c2b2d50d48cdebd5
Guilherme Pires <guilherme.pires@alleninstitute.org>
2022-04-05 10:33:50 -0700

    add missing super init call

--- serotiny/models/vae/priors/abstract_prior.py
+++ serotiny/models/vae/priors/abstract_prior.py
@@ -5,6 +5,7 @@ _full_slice = slice(None, None, None)
 
 class Prior(nn.Module):
     def __init__(self, indices=_full_slice, **kwargs):
+        super().__init__()
         self.indices = indices
         pass
 
