906e926438f31da55eef6dcb19e71dcd2e131b08
Guilherme Pires <guilherme.pires@alleninstitute.org>
2022-06-13 10:28:02 -0700

    added lr schedulers to latent_loss_vae

--- serotiny/models/vae/latent_loss_vae.py
+++ serotiny/models/vae/latent_loss_vae.py
@@ -19,8 +19,10 @@ class LatentLossVAE(BaseVAE):
         x_label: str,
         latent_loss: dict,
         latent_loss_target: dict,
+        latent_loss_weights: dict,
         latent_loss_backprop_when: dict = None,
-        latent_optimizer: torch.optim.Optimizer = torch.optim.Adam,
+        latent_loss_optimizer: torch.optim.Optimizer = torch.optim.Adam,
+        latent_loss_scheduler: torch.optim.lr_scheduler._LRScheduler = None,
         beta: float = 1.0,
         id_label: Optional[str] = None,
         optimizer: torch.optim.Optimizer = torch.optim.Adam,
@@ -52,18 +54,28 @@ class LatentLossVAE(BaseVAE):
 
         if not isinstance(latent_loss_target, (dict, DictConfig)):
             assert x_label is not None
-            latent_loss = {x_label: latent_loss_target}
-        self.latent_loss = latent_loss_target
+            latent_loss_target = {x_label: latent_loss_target}
+        self.latent_loss_target = latent_loss_target
 
-        if not isinstance(latent_optimizer, (dict, DictConfig)):
+        if not isinstance(latent_loss_weights, (dict, DictConfig)):
             assert x_label is not None
-            latent_optimizer = {x_label: latent_optimizer}
-        self.latent_optimizer = latent_loss
+            latent_loss_weights = {x_label: latent_loss_weights}
+        self.latent_loss_weights = latent_loss_weights
 
         if not isinstance(latent_loss_backprop_when, (dict, DictConfig)):
             assert x_label is not None
             latent_loss_backprop_when = {x_label: latent_loss_backprop_when}
         self.latent_loss_backprop_when = latent_loss
+
+        if not isinstance(latent_loss_optimizer, (dict, DictConfig)):
+            assert x_label is not None
+            latent_loss_optimizer = {x_label: latent_loss_optimizer}
+        self.latent_loss_optimizer = latent_loss_optimizer
+
+        if not isinstance(latent_loss_optimizer, (dict, DictConfig)):
+            assert x_label is not None
+            latent_loss_scheduler = {x_label: latent_loss_scheduler}
+        self.latent_loss_scheduler = latent_loss_scheduler
 
         self.automatic_optimization = False
 
@@ -79,23 +91,32 @@ class LatentLossVAE(BaseVAE):
             ) = self.forward(batch, decode=True, compute_loss=True)
 
             if stage == "train":
-                optimizers = self.optimizers()
+                optimizers, lr_schedulers = self.optimizers()
                 main_optim = optimizers.pop(0)
+                main_lr_sched = lr_schedulers.pop(0)
                 main_optim.zero_grad()
                 self.manual_backward(loss)
                 main_optim.step()
+                if main_lr_sched is not None:
+                    main_lr_sched.step()
 
-            for optim_ix, optim in enumerate(optimizers):
-                part = self.latent_optimizer_map[optim_ix]
+            for optim_ix, (optim, lr_sched) in enumerate(
+                zip(optimizers, lr_schedulers)
+            ):
+                part = self.latent_loss_optimizer_map[optim_ix]
                 loss = self.latent_loss[part](
                     z_parts_params[part], batch[self.latent_loss_target[part]]
                 )
+                loss = loss * self.latent_loss_weights.get(part, 1.0)
+
                 if stage == "train":
                     mod = self.latent_loss_backprop_when.get(part) or 1
                     if batch_idx % mod == 0:
                         optim.zero_grad()
                         self.manual_backward(loss)
                         optim.step()
+                        if lr_sched is not None:
+                            lr_sched.step()
 
                 on_step = stage == "train"
                 self.log(
@@ -122,18 +143,31 @@ class LatentLossVAE(BaseVAE):
 
         def configure_optimizers(self):
             optimizers = [self.optimizer(self.parameters())]
-            self.latent_optimizer_map = dict()
-            for optim_ix, (key, latent_optim) in enumerate(
-                self.latent_optimizer.items()
+
+            lr_schedulers = [
+                (
+                    self.lr_scheduler(optimizer=optimizers[0])
+                    if self.lr_scheduler is not None
+                    else None
+                )
+            ]
+
+            self.latent_loss_optimizer_map = dict()
+            for optim_ix, (part, latent_optim) in enumerate(
+                self.latent_loss_optimizer.items()
             ):
-                self.latent_optimizer_map[optim_ix] = key
+                self.latent_loss_optimizer_map[optim_ix] = part
                 optimizers.append(
                     latent_optim(
                         chain(
-                            self.encoder[key].parameters(),
-                            self.latent_loss[key].parameters(),
+                            self.encoder[part].parameters(),
+                            self.latent_loss[part].parameters(),
                         )
                     )
                 )
+                lr_sched = self.latent_loss_scheduler.get(part)
+                if lr_sched is not None:
+                    lr_sched = lr_sched(optimizer=optimizers[-1])
+                lr_schedulers.append(lr_sched)
 
-            return optimizers
+            return optimizers, lr_schedulers
