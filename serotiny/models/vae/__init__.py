6b712f722325f89946683e9303afc11c3fff5f13
Ritvik Vasan <ritvik.vasan@prd-aics-dcp-037.corp.alleninstitute.org>
2022-06-20 16:59:19 -0700

    minimum working example of latent loss vae for a CPA example dataset

--- serotiny/models/vae/__init__.py
+++ serotiny/models/vae/__init__.py
@@ -1,6 +1,6 @@
 from .image_vae import ImageVAE
 from .tabular_vae import TabularVAE
 from .implicit_decoder_vae import ImplicitDecoderVAE
-# from .conditional_image_vae import ConditionalImageVAE
+from .conditional_image_vae import ConditionalImageVAE
 from .cpa import CPA
 from .latent_loss_vae import LatentLossVAE
--- serotiny/models/vae/latent_loss_vae.py
+++ serotiny/models/vae/latent_loss_vae.py
@@ -118,6 +118,142 @@ class LatentLossVAE(BaseVAE):
             latent_covariate += z_parts[i]
         return latent_basal + latent_perturbation + latent_covariate
 
+    def _step(self, stage, batch, batch_idx, logger):
+        (
+            x_hat,
+            z_parts,
+            z_parts_params,
+            z_composed,
+            loss,
+            reconstruction_loss,
+            kld_loss,
+            kld_per_part,
+        ) = self.forward(batch, decode=True, compute_loss=True)
+        if stage == 'train':
+            print(reconstruction_loss, z_composed.max(), batch['drug_dose'].unique())
+
+        _loss = {}
+        for part in self.latent_loss.keys():
+            if isinstance(self.latent_loss[part].loss, torch.nn.modules.loss.BCEWithLogitsLoss):
+                batch[self.latent_loss_target[part]] = batch[self.latent_loss_target[part]].gt(0).float()
+
+            _loss[part] = self.latent_loss[part](
+                z_parts_params[self.hparams.x_label], batch[self.latent_loss_target[part]]
+            ) * self.latent_loss_weights.get(part, 1.0) 
+
+        optimizers = self.optimizers()
+        lr_schedulers = self.lr_schedulers()
+        main_optim = optimizers.pop(0)
+        main_lr_sched = lr_schedulers.pop(0)
+
+        non_main_key = [i for i in self.optimizer.keys() if i != 'main']
+        non_main_optims = []
+        non_main_lr_scheds = []
+        for i in non_main_key:
+            non_main_optims.append(optimizers.pop(0))
+            non_main_lr_scheds.append(lr_schedulers.pop(0))
+
+        adversarial_flag = False
+        for optim_ix, (optim, lr_sched) in enumerate(
+            zip(optimizers, lr_schedulers)
+        ):
+
+            group_key = self.latent_loss_optimizer_map[optim_ix]
+            parts = self.latent_loss_optimizer[group_key]['keys']
+            mod = self.latent_loss_backprop_when.get(group_key) or 3
+            adv_loss = 0
+            if stage == 'train':
+                for part in parts:  
+                    adv_loss += _loss[part]
+                if batch_idx % mod == 0:
+                    adversarial_flag = True
+                    optim.zero_grad()
+                    self.manual_backward(adv_loss)
+                    optim.step()
+                    # Dont use LR scheduler here, messes up the loss
+                    # if lr_sched is not None:
+                    #     lr_sched.step()
+
+                    on_step = stage == "train"
+
+                    self.log(
+                        f"{stage}_{part}_latent_loss",
+                        adv_loss.detach().cpu(),
+                        logger=logger,
+                        on_step=on_step,
+                        on_epoch=True,
+                    )
+
+        if (stage == "train") & (adversarial_flag == False):
+            main_optim.zero_grad()
+            for non_main_optim in non_main_optims:
+                non_main_optim.zero_grad()
+            self.manual_backward(loss - adv_loss)
+            main_optim.step()
+            for non_main_optim in non_main_optims:
+                non_main_optim.step()
+            # Dont use LR scheduler here, messes up the loss
+            # if main_lr_sched is not None:
+            #     main_lr_sched.step()
+            # for non_main_lr_sched in non_main_lr_scheds:
+            #     non_main_lr_sched.step()
+
+        self.log_metrics(stage, reconstruction_loss, kld_loss, loss, logger, x_hat.shape[0])
+
+        return self.make_results_dict(
+            stage,
+            batch,
+            loss,
+            reconstruction_loss,
+            kld_loss,
+            kld_per_part,
+            z_parts,
+            z_parts_params,
+            z_composed,
+            x_hat,
+        )
+
+    def configure_optimizers(self):
+
+        get_params = lambda self: list(self.parameters())
+
+        _parameters = get_params(self.decoder)
+        for part in self.optimizer['main']['keys']:
+            _parameters.extend(get_params(self.encoder[part]))
+
+        optimizers = [self.optimizer['main']['opt'](_parameters)]
+
+        lr_schedulers = [
+            (
+                self.lr_scheduler['main'](optimizer=optimizers[0])
+                if self.lr_scheduler['main'] is not None
+                else None
+            )
+        ]
+
+        non_main_key = [i for i in self.optimizer.keys() if i != 'main'][0]
+        if len(non_main_key) > 0:
+            _parameters2 = get_params(self.encoder[self.optimizer[non_main_key]['keys'][0]])
+            if len(self.optimizer[non_main_key]['keys']) > 1:
+                for key in self.optimizer[non_main_key]['keys'][1:]:
+                    _parameters2.extend(get_params(self.encoder[self.optimizer[non_main_key]['keys'][key]]))
+            optimizers.append(self.optimizer[non_main_key]['opt'](_parameters2))
+            lr_schedulers.append(self.lr_scheduler[non_main_key](optimizer=optimizers[-1]))
+
+        self.latent_loss_optimizer_map = dict()
+        for optim_ix, (group_key, group) in enumerate(self.latent_loss_optimizer.items()):
+            self.latent_loss_optimizer_map[optim_ix] = group_key
+            _parameters3 = (
+                get_params(self.latent_loss[group['keys'][0]])
+            )
+            if len(group['keys']) > 1:
+                for key in group['keys'][1:]:
+                    _parameters3.extend(get_params(self.latent_loss[key]))
+            optimizers.append(group['opt'](_parameters3))
+            lr_schedulers.append(self.latent_loss_scheduler[group_key](optimizer=optimizers[-1]))
+
+        return optimizers, lr_schedulers
+
     # def _step(self, stage, batch, batch_idx, logger):
     #     (
     #         x_hat,
@@ -149,38 +285,28 @@ class LatentLossVAE(BaseVAE):
     #     main_optim = optimizers.pop(0)
     #     main_lr_sched = lr_schedulers.pop(0)
 
-    #     adversarial_flag = False
-    #     for optim_ix, (optim, lr_sched) in enumerate(
-    #         zip(optimizers[:1], lr_schedulers[:1])
-    #     ):
-    #         part = self.latent_loss_optimizer_map[optim_ix]
+    #     opt_adv = optimizers[0]
+    #     opt_dos = optimizers[1]
 
-    #         if stage == "train":
-    #             mod = self.latent_loss_backprop_when.get(part) or 5
+    #     if (batch_idx % 3 == 0) & (stage == 'train'):
 
-    #             if batch_idx % mod == 0:
-    #                 adversarial_flag = True
-    #                 optim.zero_grad()
-    #                 self.manual_backward(_loss[part])
-    #                 optim.step()
-    #                 if lr_sched is not None:
-    #                     lr_sched.step()
-
-    #         on_step = stage == "train"
-    #         self.log(
-    #             f"{stage}_{part}_latent_loss",
-    #             _loss[part].detach().cpu(),
-    #             logger=logger,
-    #             on_step=on_step,
-    #             on_epoch=True,
+    #         adv_loss = _loss['drug'] + _loss['cell_type']
+    #         opt_adv.zero_grad()
+        
+    #         self.manual_backward(
+    #             adv_loss
     #         )
-
-    #     if (stage == "train") & (adversarial_flag == False):
+    #         opt_adv.step()
+    #     elif stage == 'train':
     #         main_optim.zero_grad()
-    #         self.manual_backward(loss - sum(_loss.values()))
+    #         opt_dos.zero_grad()
+    #         (
+    #             reconstruction_loss
+    #             - _loss['drug'] 
+    #             - _loss['cell_type']
+    #         ).backward()
     #         main_optim.step()
-    #         if main_lr_sched is not None:
-    #             main_lr_sched.step()
+    #         opt_dos.step()
 
     #     self.log_metrics(stage, reconstruction_loss, kld_loss, loss, logger, x_hat.shape[0])
 
@@ -200,157 +326,55 @@ class LatentLossVAE(BaseVAE):
     # def configure_optimizers(self):
 
     #     get_params = lambda self, cond: list(self.parameters()) if cond else []
+    #     encoders = {i: self.encoder[i] for i in ['X', 'drug', 'cell_type']}
+
     #     _parameters = (
-    #         get_params(self.encoder, True)
+    #         get_params(nn.ModuleDict(encoders), True)
     #         + get_params(self.decoder, True)
     #     )
-    #     optimizers = [self.optimizer(_parameters)]
+    #     optimizers = [self.optimizer['main']['opt'](_parameters)]
+    #     lr_schedulers = []
 
-    #     lr_schedulers = [
-    #         (
-    #             self.lr_scheduler(optimizer=optimizers[0])
-    #             if self.lr_scheduler is not None
-    #             else None
+    #     # lr_schedulers = [
+    #     #     (
+    #     #         self.lr_scheduler(optimizer=optimizers[0])
+    #     #         if self.lr_scheduler is not None
+    #     #         else None
+    #     #     )
+    #     # ]
+    #     self.latent_loss_optimizer_map = {0: 'adv'}
+    #     _parameters2 = (
+    #         get_params(self.latent_loss['drug'], True)
+    #         + get_params(self.latent_loss['cell_type'], True)
+    #     )
+
+    #     optimizers.append(self.optimizer['dose']['opt'](
+    #         self.encoder['dose'].parameters()
+    #     ))
+
+    #     optimizers.append(
+    #         self.latent_loss_optimizer['adv']['opt'](
+    #             _parameters2
     #         )
-    #     ]
+    #     )
 
-    #     self.latent_loss_optimizer_map = dict()
-    #     for optim_ix, (part, latent_optim) in enumerate(
-    #         self.latent_loss_optimizer.items()
-    #     ):
-    #         self.latent_loss_optimizer_map[optim_ix] = part
-    #         # import ipdb
-    #         # ipdb.set_trace()
-    #         optimizers.append(
-    #             latent_optim(
-    #                 self.latent_loss[part].parameters(),
-    #             )
-    #         )
-    #         lr_sched = self.latent_loss_scheduler.get(part)
-    #         if lr_sched is not None:
-    #             lr_sched = lr_sched(optimizer=optimizers[-1])
-    #         lr_schedulers.append(lr_sched)
 
+
+    #     # Now schedulers
+    #     lr_schedulers.append(torch.optim.lr_scheduler.StepLR(
+    #         optimizers[0], step_size=45
+    #     ))
+
+    #     lr_schedulers.append(torch.optim.lr_scheduler.StepLR(
+    #         optimizers[1], step_size=45
+    #     ))
+
+    #     lr_schedulers.append(torch.optim.lr_scheduler.StepLR(
+    #         optimizers[2], step_size=45
+    #     ))
+    #     # import ipdb
+    #     # ipdb.set_trace()
     #     return optimizers, lr_schedulers
-
-    def _step(self, stage, batch, batch_idx, logger):
-        (
-            x_hat,
-            z_parts,
-            z_parts_params,
-            z_composed,
-            loss,
-            reconstruction_loss,
-            kld_loss,
-            kld_per_part,
-        ) = self.forward(batch, decode=True, compute_loss=True)
-        if stage != 'train':
-            print(reconstruction_loss, z_composed.max(), batch['drug_dose'].unique())
-            # import ipdb
-            # ipdb.set_trace()
-        _loss = {}
-        for part in self.latent_loss.keys():
-            if isinstance(self.latent_loss[part].loss, torch.nn.modules.loss.BCEWithLogitsLoss):
-                batch[self.latent_loss_target[part]] = batch[self.latent_loss_target[part]].gt(0).float()
-
-            _loss[part] = self.latent_loss[part](
-                z_parts_params[self.hparams.x_label], batch[self.latent_loss_target[part]]
-            ) * self.latent_loss_weights.get(part, 1.0) 
-
-        # import ipdb
-        # ipdb.set_trace()
-        optimizers = self.optimizers()
-        lr_schedulers = self.lr_schedulers()
-        main_optim = optimizers.pop(0)
-        main_lr_sched = lr_schedulers.pop(0)
-
-        opt_adv = optimizers[0]
-        opt_dos = optimizers[1]
-
-        if (batch_idx % 3 == 0) & (stage == 'train'):
-
-            adv_loss = _loss['drug'] + _loss['cell_type']
-            opt_adv.zero_grad()
-        
-            self.manual_backward(
-                adv_loss
-            )
-            opt_adv.step()
-        elif stage == 'train':
-            main_optim.zero_grad()
-            opt_dos.zero_grad()
-            (
-                reconstruction_loss
-                - _loss['drug'] 
-                - _loss['cell_type']
-            ).backward()
-            main_optim.step()
-            opt_dos.step()
-
-        self.log_metrics(stage, reconstruction_loss, kld_loss, loss, logger, x_hat.shape[0])
-
-        return self.make_results_dict(
-            stage,
-            batch,
-            loss,
-            reconstruction_loss,
-            kld_loss,
-            kld_per_part,
-            z_parts,
-            z_parts_params,
-            z_composed,
-            x_hat,
-        )
-
-    def configure_optimizers(self):
-
-        get_params = lambda self, cond: list(self.parameters()) if cond else []
-        encoders = {i: self.encoder[i] for i in ['X', 'drug', 'cell_type']}
-
-        _parameters = (
-            get_params(nn.ModuleDict(encoders), True)
-            + get_params(self.decoder, True)
-        )
-        optimizers = [self.optimizer(_parameters)]
-        lr_schedulers = []
-
-        # lr_schedulers = [
-        #     (
-        #         self.lr_scheduler(optimizer=optimizers[0])
-        #         if self.lr_scheduler is not None
-        #         else None
-        #     )
-        # ]
-
-        _parameters2 = (
-            get_params(self.latent_loss['drug'], True)
-            + get_params(self.latent_loss['cell_type'], True)
-        )
-
-        optimizers.append(
-            self.optimizer(
-                _parameters2
-            )
-        )
-        optimizers.append(self.optimizer(
-            self.encoder['dose'].parameters()
-        ))
-
-        # Now schedulers
-        lr_schedulers.append(torch.optim.lr_scheduler.StepLR(
-            optimizers[0], step_size=45
-        ))
-
-        lr_schedulers.append(torch.optim.lr_scheduler.StepLR(
-            optimizers[1], step_size=45
-        ))
-
-        lr_schedulers.append(torch.optim.lr_scheduler.StepLR(
-            optimizers[2], step_size=45
-        ))
-        # import ipdb
-        # ipdb.set_trace()
-        return optimizers, lr_schedulers
 
     # def configure_optimizers(self):
 
