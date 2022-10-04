f0fe53ced2508bf3d89f16d3813d29d28cbbba4b
Guilherme Pires <guilherme.pires@alleninstitute.org>
2022-10-04 16:41:10 -0700

    add latent loss vae

--- serotiny/models/vae/__init__.py
+++ serotiny/models/vae/__init__.py
@@ -2,3 +2,4 @@ from .image_vae import ImageVAE
 from .tabular_vae import TabularVAE
 from .implicit_decoder_vae import ImplicitDecoderVAE
 from .base_vae import BaseVAE
+from .latent_loss_vae import LatentLossVAE
