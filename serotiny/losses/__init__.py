08fbc9ab1d6dfb26380d077efde065e672904ee8
Guilherme Pires <guilherme.pires@alleninstitute.org>
2022-10-04 12:59:49 -0700

    add gaussian nll loss

--- serotiny/losses/__init__.py
+++ serotiny/losses/__init__.py
@@ -2,3 +2,4 @@ from .continuous_bernoulli import CBLogLoss
 from .weibull import WeibullLogLoss
 from .cosine_loss import CosineLoss
 from .adversarial_loss import AdversarialLoss
+from .gaussian_nllloss import GaussianNLLLoss
