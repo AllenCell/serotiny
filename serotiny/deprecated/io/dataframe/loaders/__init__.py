c3147ed9a57e9125863ea7a0d49c057c62a17287
Guilherme Pires <guilherme.pires@alleninstitute.org>
2022-12-16 12:26:07 -0800

    deprecate

--- serotiny/deprecated/io/dataframe/loaders/__init__.py
+++
@@ -1,6 +0,0 @@
-from .classes import LoadClass
-from .columns import LoadColumn, LoadColumns
-from .image import LoadImage
-from .random_tensor import LoadRandomTensor
-
-__all__ = ["LoadColumns", "LoadColumn", "LoadClass", "LoadImage"]
--- serotiny/deprecated/networks/layers/flatten.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-
-
-class Flatten(nn.Module):
-    def forward(self, x, batch=True):
-        if batch:
-            return x.reshape(x.shape[0], -1)
-        return torch.flatten(x)
