e56ba6f55df5297645eb6aadf554bd9bbef87579
Guilherme Pires <guilherme.pires@alleninstitute.org>
2023-01-13 17:40:21 -0800

    read using anndata directly instead of scanpy

--- serotiny/dataframe/readers.py
+++ serotiny/dataframe/readers.py
@@ -10,11 +10,10 @@ except ModuleNotFoundError:
     import pandas as pd
 
 import pyarrow.parquet
-import scanpy as sc
 import anndata
 
 
-def read_h5ad(path, include_columns=None):
+def read_h5ad(path, include_columns=None, backed=None):
     """Read an annData object stored in a .h5ad file.
 
     Parameters
@@ -27,11 +26,19 @@ def read_h5ad(path, include_columns=None):
         List of column names and/or regex expressions, used to only include the
         desired columns in the resulting dataframe.
 
+    backed: Optional[str] = None
+        Can be (either "r" or "r+").
+        See anndata's docs for details:
+        https://anndata.readthedocs.io/en/latest/generated/anndata.read_h5ad.html#anndata.read_h5ad
+
     Returns
     -------
     annData
     """
-    dataframe = sc.read(path)
+
+    if backed:
+        assert backed in ("r", "r+")
+    dataframe = anndata.read_hda5(path, backed=backed)
 
     if include_columns is not None:
         columns = []
