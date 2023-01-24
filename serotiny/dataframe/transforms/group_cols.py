b60c4a121e7e818dc50d8cb40a7eea128217dad4
Guilherme Pires <mail@gpir.es>
2023-01-24 13:21:15 -0800

    Revert "fix dict iteration"
    This reverts commit 3be15f6176a5a4fafd10e967fab9994f256e7960.

--- serotiny/dataframe/transforms/group_cols.py
+++ serotiny/dataframe/transforms/group_cols.py
@@ -38,7 +38,7 @@ class GroupCols(Transform):
     def __call__(self, row):
         res = {}
 
-        for k, v in self.groups.items():
+        for k, v in self.groups:
             res.update(self._make_group(k, v, row))
 
         return res
